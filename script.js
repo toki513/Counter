a235e96add6f16188e18a0aab5e338ed44489184
Toki Tahmid Khan <122171015+toki513@users.noreply.github.com>
2024-04-18 19:14:05 +0600

    updated

--- script.js
+++ script.js
@@ -3,15 +3,26 @@ const subButtonElement = document.getElementById('counterSubs')
 const displayCount = document.getElementById('counter-display')
 
 let total = 0;
+const limit = 25;
 
 const updateDisplay = () =>{
-    
+    if(total > limit){
+        total= limit
+    }
+    if(total < 0){
+        total = 0
+    }
+    displayCount.textContent = total;
+document.body.style.setProperty('background-color',`rgb(${(total/limit) * 255}, 64, 0)`)
 }
 
 addButtonElement.addEventListener('click',()=>{
     total+=1;
+    updateDisplay();
+
 })
 
 subButtonElement.addEventListener('click',()=>{
     total-=1;
+    updateDisplay();
 })
